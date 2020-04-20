b8f742b46cc872eeea8ef940da0d7e00398d107e
Marina Reimler <marinibambini@gmail.com>
2020-04-19 17:57:21 -0700

    Implement quote machine

---
+++ script.js
@@ -0,0 +1,23 @@
+var newQuote = function () {
+
+    var request = new XMLHttpRequest()
+
+    request.open('GET', 'https://type.fit/api/quotes', true)
+
+
+
+    request.onload = function () {
+        //accessing JSON data
+        var data = JSON.parse(this.response);
+        //append random to DOM
+        var randomNum = Math.floor(Math.random() * (data.length - 1))
+        document.getElementById('text').textContent = data[randomNum].text
+        document.getElementById('author').textContent = data[randomNum].author
+
+        document.body.style.background = 'yellow'
+    }
+
+    request.send()
+}
+document.getElementById('new-quote').onclick = newQuote;
+newQuote();
